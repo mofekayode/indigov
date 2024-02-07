9e1465b9355b9e31722b6db0ffee0de7b72afa1b
mofekayode <mofekayode@gmail.com>
2024-02-06 19:23:59 -0800

    feat: patch and delete is done

--- Server/lib/controllers/ConstituentController.js
+++ Server/lib/controllers/ConstituentController.js
@@ -14,12 +14,11 @@ const getConstituents = async (_, res) => {
 };
 exports.getConstituents = getConstituents;
 const addConstituent = async (req, res) => {
-    const { email, first_name, last_name, address } = req.body; // Ensure variables match your table structure and casing
+    const { email, first_name, last_name, address } = req.body;
     try {
         let result = await index_1.db.query(`INSERT INTO public.constituents (email, first_name, last_name, address) VALUES ($1, $2, $3, $4)
  ON CONFLICT (email) DO UPDATE SET first_name = EXCLUDED.first_name, last_name = EXCLUDED.last_name, address = EXCLUDED.address
- RETURNING *;`, [email, first_name, last_name, address] // Match the placeholders to the variables correctly
-        );
+ RETURNING *;`, [email, first_name, last_name, address]);
         res.status(200).json(result.rows[0]);
     }
     catch (error) {
@@ -27,8 +26,27 @@ const addConstituent = async (req, res) => {
     }
 };
 exports.addConstituent = addConstituent;
-const updateConstituent = async (req, res) => { };
+const updateConstituent = async (req, res) => {
+    const { id } = req.params;
+    const { email, first_name, last_name, address } = req.body;
+    try {
+        let result = await index_1.db.query(`UPDATE public.constituents SET email = $1, first_name = $2, last_name = $3, address = $4 WHERE id = $5 RETURNING *;`, [email, first_name, last_name, address, id]);
+        res.status(200).json(result.rows[0]);
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+};
 exports.updateConstituent = updateConstituent;
-const deleteConstituent = async (req, res) => { };
+const deleteConstituent = async (req, res) => {
+    const { id } = req.params;
+    try {
+        let result = await index_1.db.query(`DELETE FROM public.constituents WHERE id = $1;`, [id]);
+        res.status(200).json(result.rows[0]);
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+};
 exports.deleteConstituent = deleteConstituent;
 //# sourceMappingURL=ConstituentController.js.map
--- Server/src/controllers/ConstituentController.ts
+++ Server/src/controllers/ConstituentController.ts
@@ -14,13 +14,13 @@ export const getConstituents = async (_: Request, res: Response) => {
 };
 
 export const addConstituent = async (req: Request, res: Response) => {
-  const { email, first_name, last_name, address } = req.body; // Ensure variables match your table structure and casing
+  const { email, first_name, last_name, address } = req.body; 
   try {
     let result = await db.query(
       `INSERT INTO public.constituents (email, first_name, last_name, address) VALUES ($1, $2, $3, $4)
  ON CONFLICT (email) DO UPDATE SET first_name = EXCLUDED.first_name, last_name = EXCLUDED.last_name, address = EXCLUDED.address
  RETURNING *;`,
-      [email, first_name, last_name, address] // Match the placeholders to the variables correctly
+      [email, first_name, last_name, address]
     );
     res.status(200).json(result.rows[0]);
   } catch (error) {
@@ -28,6 +28,29 @@ export const addConstituent = async (req: Request, res: Response) => {
   }
 };
 
-export const updateConstituent = async (req: Request, res: Response) => {};
+export const updateConstituent = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { email, first_name, last_name, address } = req.body;
+    try {
+        let result = await db.query(
+        `UPDATE public.constituents SET email = $1, first_name = $2, last_name = $3, address = $4 WHERE id = $5 RETURNING *;`,
+        [email, first_name, last_name, address, id]
+        );
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
-export const deleteConstituent = async (req: Request, res: Response) => {};
+export const deleteConstituent = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        let result = await db.query(
+        `DELETE FROM public.constituents WHERE id = $1;`,
+        [id]
+        );
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
